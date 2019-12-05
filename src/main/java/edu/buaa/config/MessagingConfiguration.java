da840ed7435c0a8b29d41d602d88ff2347cf071e
jwh <934114273@qq.com>
2019-12-05 17:20:22 +0800

    利用kafka更新目标信息

--- src/main/java/edu/buaa/config/MessagingConfiguration.java
+++ src/main/java/edu/buaa/config/MessagingConfiguration.java
@@ -3,6 +3,7 @@ package edu.buaa.config;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import edu.buaa.service.messaging.channel.SynchronizeTargetChannel;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,7 +21,7 @@ import org.springframework.messaging.support.GenericMessage;
  * See http://docs.spring.io/spring-cloud-stream/docs/current/reference/htmlsingle/
  * for the official Spring Cloud Stream documentation.
  */
-@EnableBinding(value = { Source.class })
+@EnableBinding(value = { Source.class,SynchronizeTargetChannel.class })
 public class MessagingConfiguration {
 
     @Value("${spring.application.name:JhipsterService}")
@@ -32,10 +33,10 @@ public class MessagingConfiguration {
      * In order to see the test messages, you can use the Kafka command-line client:
      * "./kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic topic-jhipster --from-beginning".
      */
-    @Bean
-    @InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedRate = "60000"))
-    public MessageSource<String> timerMessageSource() {
-        return () -> new GenericMessage<>("Test message from " + applicationName
-            + " sent at " + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-    }
+//    @Bean
+//    @InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedRate = "60000"))
+//    public MessageSource<String> timerMessageSource() {
+//        return () -> new GenericMessage<>("Test message from " + applicationName
+//            + " sent at " + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+//    }
 }
