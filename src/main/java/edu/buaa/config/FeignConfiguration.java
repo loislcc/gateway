4dd044c6c1b0a133ddf18f428595dfb89de6671d
jwh <934114273@qq.com>
2019-12-05 10:08:30 +0800

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/edu/buaa/config/FeignConfiguration.java
@@ -0,0 +1,18 @@
+package edu.buaa.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "edu.buaa")
+public class FeignConfiguration {
+
+    /**
+     * Set the Feign specific log level to log client REST requests
+     */
+    @Bean
+    feign.Logger.Level feignLoggerLevel() {
+        return feign.Logger.Level.BASIC;
+    }
+}
---
+++ src/main/java/edu/buaa/web/websocket/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * WebSocket services, using Spring Websocket.
+ */
+package edu.buaa.web.websocket;
