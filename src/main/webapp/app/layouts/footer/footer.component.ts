2631fdf4a115ac8842d58b9b270a1302dbe6892d
lois <loisliuchang@163.com>
2020-10-07 10:18:46 +0800

    控制台样式

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TerminalService } from 'primeng/components/terminal/terminalservice';
+import { ConsoleInfo } from 'app/app.constants';
+import { ConsoletrackerService } from 'app/service/consoletracker.service';
 
 @Component({
     selector: 'jhi-footer',
-    templateUrl: './footer.component.html'
+    templateUrl: './footer.component.html',
+    styleUrls: ['footer.scss'],
+    providers: [TerminalService]
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit, OnDestroy {
+    property: any;
+    constructor(private trackerService: ConsoletrackerService) {}
+
+    ngOnInit() {
+        let info: any = '';
+        ConsoleInfo.forEach(e => {
+            if (info === null || info === '' || info === undefined) {
+                info = e.toString();
+            } else {
+                info = info + '\n' + e.toString();
+            }
+        });
+        this.property = info;
+        this.trackerService.unsubscribe();
+        this.trackerService.disconnect();
+        this.trackerService.setTopic('console');
+        this.trackerService.connect();
+        this.trackerService.subscribe();
+        this.trackerService.receive().subscribe(data => {
+            console.log(data.brief);
+            const tmp = new Date() + ': ' + data.brief;
+            ConsoleInfo.push(tmp);
+            this.addData(tmp);
+        });
+    }
+
+    addData(tmp) {
+        this.property = this.property + '\n' + tmp;
+    }
+
+    ngOnDestroy() {
+        this.trackerService.unsubscribe();
+        this.trackerService.disconnect();
+    }
+}
