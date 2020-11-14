84c76801e02803d48639656f03a97c5f20925ad7
lois <loisliuchang@163.com>
2020-11-14 19:52:51 +0800

    修改

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -6,7 +6,8 @@ import { Message } from 'primeng/api';
 
 @Component({
     selector: 'jhi-main',
-    templateUrl: './main.component.html'
+    templateUrl: './main.component.html',
+    styleUrls: ['main.scss']
 })
 export class JhiMainComponent implements OnInit {
     msgs: Message[] = [];
